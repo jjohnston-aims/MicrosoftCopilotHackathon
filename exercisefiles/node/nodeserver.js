715f37c16b061dee40441512b574330260b2bace
Jared Johnston <j.johnston@aims.gov.au>
2024-06-26 12:49:45 +1000

    Start working on node challenge

--- exercisefiles/node/nodeserver.js
+++ exercisefiles/node/nodeserver.js
@@ -5,3 +5,89 @@
 // if the url has other methods, return "method not supported"
 // when server is listening, log "server is listening on port 3000"
 
+const http = require('http');
+const url = require('url');
+const colors = require('./colors.json');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const query = parsedUrl.query;
+
+    if (req.method !== 'GET') {
+        res.end('method not supported');
+    } else {
+        const path = parsedUrl.pathname;
+        // TODO: log the path in a way that is visibile when runing mocha tests
+        console.log("path: " + path);
+        switch (path) {
+            case '/get':
+                if (!query.key) {
+                    res.end('key not passed');
+                } else {
+                    res.end('hello ' + query.key);
+                }
+                break;
+            case '/DaysBetweenDates':
+                date1 = new Date(query.date1);
+                date2 = new Date(query.date2);
+                const diffTime = Math.abs(date2 - date1);
+                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                res.end(diffDays.toString());
+                break;
+            case '/Validatephonenumber':
+                if (!query.phoneNumber) {
+                    res.end('no phoneNumber passed');
+                    break;
+                } else {
+                    query.phoneNumber.length == 12 ? res.end('valid') : res.end('invalid');
+                }
+                break;
+            case '/ValidateSpanishDNI':
+                if (!query.dni) {
+                    res.end('no dni passed');
+                    break;
+                } else {
+                    const dni = query.dni;
+                    const dniNumber = dni.slice(0, -1);
+                    const dniLetter = dni.slice(-1).toUpperCase();
+                    const validLetters = 'TRWAGMYFPDXBNJZSQVHLCKE';
+                    const calculatedLetter = validLetters.charAt(dniNumber % 23);
+                    if (dniLetter === calculatedLetter) {
+                        res.end('valid');
+                    } else {
+                        res.end('invalid');
+                    }
+                }
+                break;
+            case '/ReturnColorCode':
+                if (!query.color) {
+                    res.end('no color passed');
+                } else {
+                    fs.readFile(path.join(__dirname, 'colors.json'), 'utf8', (err, data) => {
+                        if (err) {
+                            res.writeHead(500);
+                            res.end('Server error');
+                            return;
+                        }
+                        const colors = JSON.parse(data);
+                        const colorEntry = colors.find(c => c.color.toLowerCase() === query.color.toLowerCase());
+                        if (colorEntry) {
+                            res.end(JSON.stringify(colorEntry.code.rgba));
+                        } else {
+                            res.end('color not supported');
+                        }
+                    });
+                }
+                break;
+            default:
+                res.end('path not supported');
+                break;
+        }
+    }
+});
+
+server.listen(3000, () => {
+    console.log('server is listening on port 3000');
+});
--- exercisefiles/node/test.js
+++ exercisefiles/node/test.js
@@ -4,6 +4,7 @@
 //command to run this test file
 //mocha test.js
 
+const axios = require('axios');
 const assert = require('assert');
 const http = require('http');
 const server = require('./nodeserver');
@@ -23,17 +24,66 @@ describe('Node Server', () => {
         });
     });
 
+    it('should return days between dates if date1 and date2 are passed in the query string', () => {
+        http
+        .get('http://localhost:3000/DaysBetweenDates?date1=2022-01-01&date2=2022-01-02', (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, '1');
+            });
+        });
+    });
+
+    it('should validate a valid phone number', () => {
+        http
+        .get('http://localhost:3000/Validatephonenumber?phoneNumber=+34666777888', (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'valid');
+            });
+        });
+    });
+  
+    
+    it('should validate an invalid phone number', () => {
+        http
+        .get('http://localhost:3000/Validatephonenumber?phoneNumber=+3466677788', (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'invalid');
+            });
+        });
+    });
+
     //add test to check get when key is equal to world
 
     //add test to check validatephoneNumber
 
     //write test to validate validateSpanishDNI
+    it('should validate a valid spanish DNI', () => {
+        http
+        .get('http://localhost:3000/ValidateSpanishDNI?dni=12345678Z', (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                assert.equal(data, 'valid');
+            });
+        });
+    });
    
 
     //write test for returnColorCode red should return code #FF0000
-
-
-   //write test for daysBetweenDates
 
 
 
